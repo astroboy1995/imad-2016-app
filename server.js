ee6b68f28b55544e8998a134066afb5b3ef830bc
astroboy1995 <biswarup525@gmail.com>
2016-09-25 19:35:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/article-one',function(req,res){
     res.send('Article one has been requested and will be searved heare');
-}
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
