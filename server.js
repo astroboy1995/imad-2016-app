9ae3498de33d1558a9e74a177698c62552d75d99
astroboy1995 <biswarup525@gmail.com>
2016-09-26 22:46:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,14 +19,51 @@ var articleone = {
     </pr>`
 };
 
-
-
+function createTemplate (data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+    
+    <head>
+    <title>
+        ${title}
+    </title>
+    <meta name="viewport" content="width=device-width initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+    
+    </head> 
+    
+    <body>
+     <div class="container">
+         <div>
+            <a href="/">home</a>
+         </div>
+    
+         <h3>
+             ${heading}
+         </h3>
+         <div>
+             ${date}
+         </div>
+         <div>
+            ${content}    
+        </div>
+    </div>
+    </body>
+    </html>
+    `;
+    
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/ui/style.css', function (req, res) {
